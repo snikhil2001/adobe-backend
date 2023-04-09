4dffef56ae63e31887931c7ca8dca14a31a71e09
snikhil2001 <nikhilsahni2001@gmail.com>
2023-04-09 20:25:12 +0530

    modified

--- src/controllers/postControllers/unlikePost.js
+++ src/controllers/postControllers/unlikePost.js
@@ -9,6 +9,10 @@ const unlikePost = async (req, res) => {
     return res.status(404).send({ message: "Post not found" });
   }
 
+  if (post.likes === 0) {
+    return res.status(404).send({ message: "You cannot dislike now" });
+  }
+
   let likedPost = await Post.findByIdAndUpdate(id, { likes: post.likes - 1 });
   return res.status(200).send({ message: "You have unliked a post" });
 };
