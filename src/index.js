5d7e34767aca1c204e9635795bafd28edccf1d2e
snikhil2001 <nikhilsahni2001@gmail.com>
2023-04-10 08:46:25 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://adode.vercel.app/",
+  })
+);
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/analytics", analyticsRouter);
